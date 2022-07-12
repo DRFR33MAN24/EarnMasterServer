48ec8bd1d243d9ca9e8289c2314e0f9a5ca17b3a
Gordon Freeman <drfr33man@protonmail.com>
2022-07-12 15:42:28 +0300

    fixing server crashes

--- routes/api/authUsers.js
+++ routes/api/authUsers.js
@@ -32,10 +32,7 @@ const registrationSchema = {
       },
     },
   },
-  gender: {
-    notEmpty: true,
-    errorMessage: "Gender field cannot be empty",
-  },
+
   password: {
     isStrongPassword: {
       minLength: 8,
@@ -46,10 +43,44 @@ const registrationSchema = {
     errorMessage:
       "Password must be greater than 8 and contain at least one uppercase letter, one lowercase letter, and one number",
   },
-  phone: {
-    notEmpty: true,
-    errorMessage: "Phone number cannot be empty",
+  repeat_password: {
+    isStrongPassword: {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+    },
+    errorMessage:
+      "Password must be greater than 8 and contain at least one uppercase letter, one lowercase letter, and one number",
   },
+
+  email: {
+    normalizeEmail: true,
+    custom: {
+      options: (value) => {
+        return User.find({
+          email: value,
+        }).then((user) => {
+          if (user.length > 0) {
+            return Promise.reject("Email address already taken");
+          }
+        });
+      },
+    },
+  },
+};
+const loginScheme = {
+  password: {
+    isStrongPassword: {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+    },
+    errorMessage:
+      "Password must be greater than 8 and contain at least one uppercase letter, one lowercase letter, and one number",
+  },
+
   email: {
     normalizeEmail: true,
     custom: {
